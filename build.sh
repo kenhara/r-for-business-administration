011afe80e543c542b9fa99fffff2a39a5a1d5bbc
Jeff Moe <moe@alephobjects.com>
2015-04-25 23:34:07 -0600

    Add Network Overview diagram done in calligraflow with sources.
    	modified:   AOOM.pdf
    	modified:   build.sh
    	new file:   calligraflow/NiceNetwork-v1.0.tar.gz
    	new file:   calligraflow/ao-network-overview.odg
    	modified:   source/IT.tex
    	new file:   source/images/ao-network-overview.pdf

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@ cd source
 xelatex AOOM.tex
 
 # Extract PDFs for easy access
-#pdfjoin AOOM.pdf 20 -o AOOM-network.pdf
-#pdfjoin AOOM.pdf 29 -o AOOM-org-chart.pdf
-pdfjoin AOOM.pdf 31 -o AOOM-weekly-meetings.pdf
-#pdfjoin AOOM.pdf 32 -o AOOM-company-monthly.pdf
+#
+# pdfjoin AOOM.pdf 20 -o AOOM-network.pdf
+# pdfjoin AOOM.pdf 29 -o AOOM-org-chart.pdf
+# pdfjoin AOOM.pdf 31 -o AOOM-weekly-meetings.pdf
+# pdfjoin AOOM.pdf 32 -o AOOM-company-monthly.pdf
 mv *.pdf ../
 
 # Sync to github
-#git push
+# git push
 
 # Sync github to devel.alephobjects.com
-#ssh aleph@jebstation /home/aleph/bin/AO-git-pull-AOOM
+# ssh aleph@jebstation /home/aleph/bin/AO-git-pull-AOOM
 
