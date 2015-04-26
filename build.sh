de476fc41870853eeb441530b361890dd6a10e92
Jeff Moe <moe@alephobjects.com>
2015-04-25 19:53:12 -0600

    Add build scriptlet. Extract meetings as separate PDFs too.
    	new file:   AOOM-company-monthly.pdf
    	deleted:    AOOM-schedule.pdf
    	new file:   AOOM-weekly-meetings.pdf
    	modified:   AOOM.pdf
    	new file:   build.sh
    	modified:   source/SchedWeek.tex

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+cd source
+xelatex AOOM.tex
+#pdfjoin AOOM.pdf 20 -o AOOM-network.pdf
+#pdfjoin AOOM.pdf 29 -o AOOM-org-chart.pdf
+pdfjoin AOOM.pdf 31 -o AOOM-weekly-meetings.pdf
+pdfjoin AOOM.pdf 32 -o AOOM-company-monthly.pdf
+mv *.pdf ../
+
