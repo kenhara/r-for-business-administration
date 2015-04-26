2d2e1665fbe3930fded3d5aa2c6df0b090aee7fe
Jeff Moe <moe@alephobjects.com>
2015-04-25 20:07:25 -0600

    Fix build annoyance with imported network pdf.
    	modified:   AOOM-company-monthly.pdf
    	modified:   AOOM-weekly-meetings.pdf
    	modified:   AOOM.pdf
    	modified:   build.sh
    	modified:   source/IT.tex

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd source
 xelatex AOOM.tex
 #pdfjoin AOOM.pdf 20 -o AOOM-network.pdf
 #pdfjoin AOOM.pdf 29 -o AOOM-org-chart.pdf
-pdfjoin AOOM.pdf 31 -o AOOM-weekly-meetings.pdf
-pdfjoin AOOM.pdf 32 -o AOOM-company-monthly.pdf
+#pdfjoin AOOM.pdf 31 -o AOOM-weekly-meetings.pdf
+#pdfjoin AOOM.pdf 32 -o AOOM-company-monthly.pdf
 mv *.pdf ../
 
